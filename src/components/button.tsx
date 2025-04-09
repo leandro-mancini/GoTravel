775fc588d094e0ac11ad700cfc5c0a17e5c1d6df
leandro-mancini <mancinilele@gmail.com>
2025-04-09 14:09:15 -0300

    feat: update onboarding

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,27 +1,53 @@
-import React from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import React, { ReactNode } from 'react'
+import { Text } from 'react-native'
 import styled from '@emotion/native'
-import { useTheme } from '@emotion/react'
+import Ripple from "react-native-material-ripple";
+import { colors } from '../theme/colors';
 
-const ButtonContainer = styled(TouchableOpacity)(({ theme }) => ({
-  backgroundColor: theme.colors.primary[100],
+type ButtonProps = {
+  label: string
+  onPress: () => void
+  variant?: 'solid' | 'outline',
+  children?: ReactNode
+}
+
+const getContainerStyle = (variant: ButtonProps['variant'], theme: any) => {
+  switch (variant) {
+    case 'outline':
+      return {
+        backgroundColor: 'transparent',
+        borderWidth: 1,
+        borderColor: "#CECECE",
+      }
+    case 'solid':
+    default:
+      return {
+        backgroundColor: theme.colors.primary[100],
+      }
+  }
+}
+
+const ButtonContainer = styled(Ripple)<{ variant: ButtonProps['variant'] }>(({ theme, variant }) => ({
   height: 58,
   borderRadius: 50,
-  alignItems: "center",
-  justifyContent: "center"
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: 20,
+  flexDirection: "row",
+  ...getContainerStyle(variant ?? 'solid', theme),
 }))
 
-const ButtonText = styled(Text)(({ theme }) => ({
-  color: theme.colors.white,
-  ...theme.textStyle.buttonText[1]
+const ButtonText = styled(Text)<{ variant: ButtonProps['variant'] }>(({ theme, variant }) => ({
+  color: variant === 'outline' ? "#181818" : theme.colors.white,
+  ...theme.textStyle.buttonText[3],
+  fontFamily: theme.typography.fontFamily.medium
 }))
 
-export const Button = ({ title, onPress }: { title: string; onPress: () => void }) => {
-  const theme = useTheme()
-
+export const Button = ({ label, onPress, variant = 'solid', children }: ButtonProps) => {
   return (
-    <ButtonContainer onPress={onPress}>
-      <ButtonText>{title}</ButtonText>
+    <ButtonContainer onPress={onPress} variant={variant} rippleContainerBorderRadius={50}>
+      {children}
+      <ButtonText variant={variant}>{label}</ButtonText>
     </ButtonContainer>
   )
 }
--- src/screens/onboarding/screen.tsx
+++ src/screens/onboarding/screen.tsx
@@ -1,26 +1,107 @@
 import { useTheme } from '@emotion/react';
-import React from 'react'
-import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import { StyleSheet, ImageBackground, Animated, Text, View } from 'react-native'
+import { Button, Icon } from '../../components';
+
+const AnimatedImageBackground = Animated.createAnimatedComponent(ImageBackground)
 
 export const OnboardingScreen = () => {
   const theme = useTheme();
   const styles = createStyles(theme);
+  const scale = useRef(new Animated.Value(1.2)).current;
+  const helloAnim = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    Animated.timing(scale, {
+      toValue: 1,
+      duration: 800,
+      useNativeDriver: true,
+    }).start()
+
+    Animated.stagger(200, [
+      animateFadeSlide(helloAnim),
+    ]).start();
+  }, [])
 
   return (
-    <ImageBackground
-      source={require("../../../assets/images/bg-onboarding.png")}
-      style={styles.background}
-      resizeMode="cover"
-    >
+    <View style={styles.wrapper}>
+      {/* Background animado separado */}
+      <AnimatedImageBackground
+        source={require("../../../assets/images/bg-onboarding.png")}
+        style={[styles.background, { transform: [{ scale }] }]}
+        resizeMode="cover"
+      />
 
-    </ImageBackground>
+      {/* Conteúdo sobre o fundo */}
+      <View style={styles.container}>
+        <Animated.View style={[styles.hello, animatedTopStyle(helloAnim)]}>
+          <Text style={styles.title}>Bem-vindo</Text>
+          <Text style={styles.description}>
+            Explore a nova experiência com a GoTravel
+          </Text>
+        </Animated.View>
+        <View style={styles.buttons}>
+          <Button variant="outline" label="Continuar com o Google" onPress={() => {}}>
+            <Icon name="Google" size={20} />
+          </Button>
+          <Button variant="outline" label="Continuar com o Facebook" onPress={() => {}}>
+            <Icon name="Facebook" size={20} />
+          </Button>
+        </View>
+      </View>
+    </View>
   )
 }
 
+const animateFadeSlide = (animatedValue: Animated.Value) =>
+  Animated.timing(animatedValue, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+  })
+
+const animatedTopStyle = (animatedValue: Animated.Value) => ({
+  opacity: animatedValue,
+  transform: [
+      {
+          translateY: animatedValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [20, 0],
+          }),
+      },
+  ],
+})
+
 const createStyles = (theme: ReturnType<typeof useTheme>) =>
   StyleSheet.create({
+    wrapper: {
+      flex: 1,
+    },
     background: {
-      width: "100%",
-      height: "100%"
+      ...StyleSheet.absoluteFillObject,
+    },
+    container: {
+      gap: 30,
+      paddingTop: 60
+    },
+    hello: {
+      alignItems: "center",
+      gap: 12,
+    },
+    title: {
+      fontSize: 40,
+      lineHeight: 42,
+      fontFamily: theme.typography.fontFamily.medium,
+      color: theme.colors.dark.primary[100]
+    },
+    description: {
+      fontSize: 16,
+      lineHeight: 18,
+      fontFamily: theme.typography.fontFamily.regular,
+      color: theme.colors.text.secondary[100]
+    },
+    buttons: {
+      paddingHorizontal: 40,
+      gap: 16
     },
   })
