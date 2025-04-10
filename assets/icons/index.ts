d2843f5cf4887fd6a3a99278714623fddd2a46ab
leandro-mancini <mancinilele@gmail.com>
2025-04-09 21:48:51 -0300

    feat: add icons bottom navigatior

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,3 +1,12 @@
+export { default as BookmarkFilled } from './bookmark-filled.svg';
+export { default as Bookmark } from './bookmark.svg';
+export { default as DiscoveryFilled } from './discovery-filled.svg';
+export { default as Discovery } from './discovery.svg';
 export { default as Facebook } from './facebook.svg';
 export { default as Google } from './google.svg';
+export { default as HomeFilled } from './home-filled.svg';
+export { default as Home } from './home.svg';
 export { default as Logo } from './logo.svg';
+export { default as Search } from './search.svg';
+export { default as SettingFilled } from './setting-filled.svg';
+export { default as Setting } from './setting.svg';
